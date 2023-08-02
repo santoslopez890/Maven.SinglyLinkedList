7ada67af44eccacb23a005be06726eab3bbba8c4
santos <santoslopez890@gmail.com>
2023-08-02 11:31:51 -0400

    add tests and tostring works

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -9,9 +9,60 @@ public class SinglyLinkedList<T> implements LinkedInterface {
 public SinglyLinkedList(){
 
     }
+    private Node<T> head =null;
+    public Boolean isEmpty(){
+    return head == null;
+    }
     @Override
     public void add(Object data) {
+    Node newNode= new Node(data);
+    if(this.head == null){
+        this.head =newNode;
+    }
+    else{
+        Node<T> temp = head;
+        while(temp.hasNext()){
+            temp=temp.getNext();
+        }
+        temp.setNext(newNode);
+    }
+    }
 
+    public void add2(Object data,int delete) {
+        Node newnode = new Node(data);
+        if(this.head == null){
+            this.head = newnode;
+        }
+        else{
+            Node<T> temp = head;
+            while(temp.hasNext()){ // walk to end
+                temp = temp.getNext();
+            }
+            temp.setNext(newnode);
+        }
+
+    }
+    @Override
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+
+        //ADD HEAD
+        sb.append("Head -> ");
+
+        //Loop through the list and print each data
+        if(head  != null) {
+            Node<T> temp = head;
+            do { // walk to end
+                //Print the data.....
+                sb.append("[" + temp.getData() + "] -> ");
+                temp = temp.getNext();
+            } while (temp != null);  //&& temp.hasNext());
+        }
+
+        //ADD NULL
+        sb.append("NULL");
+
+        return sb.toString();
     }
 
     @Override
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,60 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void testConstructor(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<>();
+        Assert.assertNotNull(llist);
+    }
+    @Test
+    public void testIsEmpty(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<Integer>();
+        Assert. assertEquals( true, llist.isEmpty());
+    }
+    @Test
+    public void testAdd1(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<Integer>();
+        llist.add(1);
+        Assert. assertEquals( false, llist.isEmpty());
+    }
+    @Test
+    public void testAdd2(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<Integer>();
+        llist.add(1);
+        llist.add(2);
+        Assert. assertEquals( false, llist.isEmpty());
+    }
+    @Test
+    public void testToString(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<Integer>();
+      String expected = "Head -> NULL";
+      Assert.assertEquals(expected,llist.toString());
+
+    }
+    @Test
+    public void testToString2(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<Integer>();
+        llist.add(1);
+        String expected = "Head -> [1] -> NULL";
+        Assert.assertEquals(expected,llist.toString());
+
+    }
+    @Test
+    public void testToString3(){
+        SinglyLinkedList<Integer> llist=new SinglyLinkedList<Integer>();
+        llist.add(1);
+        llist.add(2);
+        String expected = "Head -> [1] -> [2] -> NULL";
+        Assert.assertEquals(expected,llist.toString());
+
+    }
+
+
 }
